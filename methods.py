ff6101c48acf5146607df7c350a5ced931bbb195
ananasness <ananasness@gmail.com>
2018-04-21 10:19:38 +0300

    Add heavy ball and backtracking

--- methods.py
+++ methods.py
@@ -79,17 +79,8 @@ def ang(v1, v2):
     sinang = np.linalg.norm(np.cross(v1, v2))
     return np.arctan2(sinang, cosang)
 
-def proj_ball(x, center, r): 
-    
-    if x[0]** 2 + x[1]**2 <= r**2:
-        return x
-        
-    
-    u = x - center
-    v = [1, 0]
-    angle = ang(v, u)
-    
-    return [center[0] - r *np.cos(angle),center[1] - r*np.sin(angle)]
+def proj_ball(x, d, r):
+    return r * (x - d) / np.linalg.norm(x - d) + d
 
 def proj_ball_desc(x0, f, f_grad, center, r, **params):
     x = x0.copy()
@@ -108,5 +99,64 @@ def proj_ball_desc(x0, f, f_grad, center, r, **params):
         f_array.append(f(x))
         
     return np.array(x_array), np.array(f_array)
+
+def heavy_ball(x0, f, f_grad, **params):
+    x = np.array(x0.copy())
+    prev_x = x.copy()
+    x_array = [x]
+    f_array = [ f(x) ]
+    iters = params.get('iters', None) or 100
+    alpha = params.get('alpha', None) or 0.2
+    beta = params.get('beta', None) or 0.65
+    
+    for k in range(iters):
+        p = np.zeros_like(x0)
+        coord = k % len(x0)
+        p[coord] = 1
+        
+        grad = f_grad(x)
+        
+        new_x = x + alpha * (-grad) + beta * (x - prev_x)
+        prev_x = x
+        x = new_x
+        
+        x_array.append(x)
+        f_array.append(f(x))
+    return np.array(x_array), np.array(f_array)
+
+
+def backtrack(x, f, f_grad, alpha, beta):
+    grad = f_grad(x)
+    gamma = 0.5
+    delta_x = gamma * (-grad)
+    while f(x + delta_x) > f(x) + alpha * gamma* np.dot(grad, delta_x):
+        gamma = beta * gamma
+    
+    return gamma
+
+def back_descent(x0, f, f_grad, **params):
+    x = np.array(x0).copy()
+    x_array = [x]
+    f_array = [ f(x) ]
+    iters = params.get('iters', None) or 100
+    alpha = params.get('alpha', None) or 0.5
+    beta = params.get('beta', None) or 0.5
+    
+    for k in range(iters):
+        p = np.zeros_like(x0)
+        coord = k % len(x0)
+        p[coord] = 1
+        
+        grad = f_grad(x)
+        
+        gamma = backtrack(x, f, f_grad, alpha, beta)
+        delta_x = gamma * (-grad)
+        
+        x = x + delta_x
+        
+        x_array.append(x)
+        f_array.append(f(x))
+    return np.array(x_array), np.array(f_array)
+        
                 
         
--- plotting.py
+++ plotting.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 from pylab import contour, cm
 import numpy as np
 
-def plot_it(func, x_star=[0,0], x_0=None, search_line=None, bound_line=None):
+def plot_it(func, x_star=[0,0], x_0=None, search_line=None, bound_line=None, **params):
+    figsize = params.get('figsize', (10,10))
     n_steps = 100
     step = 0.1
 
@@ -12,7 +13,7 @@ def plot_it(func, x_star=[0,0], x_0=None, search_line=None, bound_line=None):
     X, Y = np.meshgrid(space_x, space_y)
     Z = f(X, Y)
     
-    f, ax = plt.subplots(1, 1, figsize=(14, 7))
+    f, ax = plt.subplots(1, 1, figsize=figsize)
     
     levels = func(x_star) + np.linspace(0, 14, 14)**2
     
@@ -51,4 +52,34 @@ def plot_surf(func, x_star, **params):
     ax.contour(X, Y, Z, levels=levels, cmap=cm.GnBu, extent=[0, 1, 0, 1])
     
     return f
+
+def approx_it(y):
+    n = len(y)
+    x = np.arange(1, n+1)
+    norm = np.linalg.norm
+    E = np.sum
+    
+
+    coef = 1 / (n * norm(x)**2 - E(x)**2)
+    A = np.array([[n, -E(x)], 
+                  [-E(x), norm(x)**2]])
+    b = np.array([E(x*y), E(y)])
+    
+    return coef * np.dot(A, b)
+
+def plot_convergence(fs, optimum):
+    
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (13,4))
+    xs = np.arange(1, len(fs)+1)
+    ax1.plot(xs, (fs - optimum), 'x')
+    ax2.plot(xs, np.log(fs - optimum), 'x')
+    
+    [a, b] = approx_it(np.log(fs - optimum))
+
+    approx = np.vectorize(lambda k: np.exp(a * k  + b))
+    ks = np.linspace(1, len(fs)+1, len(fs)*2)
+    ax1.plot(ks, approx(ks), ':')
+    ax2.plot(xs, a * xs +  b, ':')
+
+    return fig
     
